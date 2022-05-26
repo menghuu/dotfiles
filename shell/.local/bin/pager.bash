37ab9689c5c7639ffd01e6aac62834b068b27582
hu meng <mail@meng.hu>
2022-05-26 20:20:08 +0800

    feat(bin): add delta or less pager

---
+++ shell/.local/bin/pager.bash
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# @see https://stackoverflow.com/questions/19408649/pipe-input-into-a-script
+if command -v delta >/dev/null 2>&1; then
+    cat | delta "$@"
+else
+    cat | less -F -X
+fi
